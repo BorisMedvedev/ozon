95857cb4fa6138bdd4f604d4d3f3bdf73008b591
Your Name <you@example.com>
2022-05-15 20:51:38 +0300

    999

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
-alert('hello');
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  //чекбокс===========================
+  const checkBox = document.querySelectorAll(".filter-check_checkbox");
+
+  checkBox.forEach((e) => {
+    e.addEventListener("change", () => {
+      if (this.checked) {
+        this.nextElementSibling.classList.add("checked");
+      } else {
+        this.nextElementSibling.classList.remove("checked");
+      }
+    });
+  });
+  //end чекбокс=================================
+
+  // корзина==========================
+  const btnCart = document.getElementById("cart");
+  const modalCart = document.querySelector(".cart");
+  const closeBtn = document.querySelector(".cart-close");
+
+  btnCart.addEventListener("click", () => {
+    modalCart.style.display = "flex";
+    document.body.style.overflow = "hidden";
+  });
+  closeBtn.addEventListener("click", () => {
+    modalCart.style.display = "none";
+    document.body.style.overflow = "";
+  });
+
+  // end корзина ===========================
+  //======================================================
+  // работа с корзина=========================================
+  const cards = document.querySelectorAll(".goods .card"),
+    cartWrapper = document.querySelector(".cart-wrapper"),
+    countGoods = document.querySelector(".counter"),
+    cartEmpty = document.getElementById("cart-empty");
+  cards.forEach((card) => {
+    const btn = card.querySelector("button");
+
+    btn.addEventListener("click", () => {
+      const cardClone = card.cloneNode(true);
+      cartWrapper.appendChild(cardClone);
+      cartEmpty.remove();
+      showData();
+    });
+  });
+
+  function showData(params) {
+    const cardsCart = cartWrapper.querySelectorAll(".card");
+    countGoods.textContent = cardsCart.length;
+  }
+
+  // end работа с корзина=========================================
+});
+
+// document.addEventListener("click", (e) => console.log(e.target));
